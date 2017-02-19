35b554e5598dad58e1069a586d89b7e13ab5c643
Raul Robledo <riul88 at gmail.com>
2017-02-19 15:50:23 -0600

    Splitted project selenium-csharp-project to allow reusability

---
+++ Testing.Common/LogFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+//Copyright (C) 2017 Raul Robledo <raul.robledo at acm.org>
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+namespace Testing.Common
+{
+    public interface ILogger
+    {
+        void message(string info);
+        void logException(Exception ex);
+    }
+
+    public class ConsoleLog : ILogger
+    {
+        public void message(string info)
+        {
+            Console.WriteLine(info);
+        }
+        public void logException(Exception ex)
+        {
+            var tmp = ex;
+            while (tmp != null)
+            {
+                message(String.Format("{0} {1}", ex.Message, ex.StackTrace));
+                tmp = tmp.InnerException;
+                if (ex == tmp) break;
+            }
+        }
+    }
+
+    public class LogFactory
+    {
+        private static ILogger log;
+
+        public static ILogger GetLogger()
+        {
+            if (log == null)
+                log = new ConsoleLog();
+            return log;
+        }
+    }
+}
---
+++ Testing.Common/TestCase.cs
@@ -0,0 +1,228 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Remote;
+using System;
+using Testing.Common;
+
+//Copyright (C) 2017 Raul Robledo <raul.robledo at acm.org>
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+namespace Testing.Common
+{
+    public class TestCase
+    {
+        #region Internal variables
+        protected TestingSection Config;
+        protected static ILogger LOG = LogFactory.GetLogger();
+        protected IWebDriver _driver;
+        public IWebDriver driver
+        {
+            get
+            {
+                return _driver;
+            }
+        }
+        public string BaseUrl
+        {
+            get
+            {
+                return Config.BaseUrl;
+            }
+        }
+        #endregion
+
+        public TestCase()
+        {
+            Config = (Testing.Common.TestingSection)System.Configuration.ConfigurationManager.GetSection("testing");
+        }
+
+        #region Test init and cleaup
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = BuildOptions();
+            BuildDriver(options);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+        #endregion
+
+        #region Helpers
+
+        public DriverOptions BuildOptions()
+        {
+            DriverOptions options;
+            switch (Config.Browser)
+            {
+                case "chrome":
+                    options = new ChromeOptions();
+                    break;
+                case "ie":
+                    options = new InternetExplorerOptions();
+                    break;
+                default:
+                    options = new FirefoxOptions();
+                    if (Config.Firefox.ElementInformation.IsPresent)
+                    {
+                        ((FirefoxOptions)options).BrowserExecutableLocation = Config.Firefox.Path;
+                    }
+                    break;
+            }
+            return options;
+        }
+
+        public void BuildDriver(DriverOptions options)
+        {
+            if (!Config.Remote.ElementInformation.IsPresent)
+            {
+                BuildDriverLocal(options);
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(Config.Remote.Url))
+                    throw new Exception("Missing remote server Url");
+                BuildDriverRemote(options);
+            }
+            _driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(Config.Driver.ImplicitlyWait));
+            _driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(Config.Driver.SetPageLoadTimeout));
+            _driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(Config.Driver.SetScriptTimeout));
+        }
+
+        private void BuildDriverLocal(DriverOptions options)
+        {
+            switch (Config.Browser)
+            {
+                case "chrome":
+                    _driver = new ChromeDriver((ChromeOptions)options);
+                    break;
+                case "ie":
+                    _driver = new InternetExplorerDriver((InternetExplorerOptions)options);
+                    break;
+                default:
+                    _driver = new FirefoxDriver((FirefoxOptions)options);
+                    break;
+            }
+        }
+
+        private void BuildDriverRemote(DriverOptions options)
+        {
+            ICapabilities capabilities;
+            switch (Config.Browser)
+            {
+                case "chrome":
+                    capabilities = DesiredCapabilities.Chrome();
+                    break;
+                case "ie":
+                    capabilities = DesiredCapabilities.InternetExplorer();
+                    break;
+                default:
+                    capabilities = DesiredCapabilities.Firefox();
+                    break;
+            }
+            _driver = new OpenQA.Selenium.Remote.RemoteWebDriver(new Uri(Config.Remote.Url), capabilities);
+        }
+
+        public bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch { }
+            return false;
+        }
+        //<script type="text/javascript">
+        //    window.onerror=function(msg){
+        //        document.body.setAttribute("JSError", msg);
+        //    }
+        //</script>
+        public void AssertNoJavaScriptError()
+        {
+            Assert.IsFalse(IsElementPresent(By.XPath("//body[@JSError]")), string.Format("JSError found in the page {0}", driver.Url));
+        }
+
+        public void AssertJavaScriptError(bool removeAttribute = true)
+        {
+            Assert.IsFalse(IsElementPresent(By.XPath("//body[@JSError]")), string.Format("JSError not found in the page {0}", driver.Url));
+            if (removeAttribute)
+            {
+                ((IJavaScriptExecutor)driver).ExecuteScript(@"document.getElementsByTagName('body')[0].removeAttribute('JSError');");
+            }
+        }
+
+        public IWebElement Find(By by, string label = null)
+        {
+            try
+            {
+                return driver.FindElement(by);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(string.Format("Unable to find element {0}", label), ex);
+            }
+        }
+
+        public void Click(By by, string label = null)
+        {
+            Click(Find(by, label), label);
+            AssertNoJavaScriptError();
+        }
+
+        public void Click(IWebElement element, string label = null)
+        {
+            try
+            {
+                element.Click();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(string.Format("Unable to click element {0}", label), ex);
+            }
+        }
+
+        public void MouseOver(By by, string label = null)
+        {
+            MouseOver(Find(by), label);
+        }
+
+        public void MouseOver(IWebElement element, string label = null)
+        {
+            if (driver is RemoteWebDriver)
+            {
+                throw new Exception("MouseOver not available for RemoteWebDriver");
+            }
+
+            try
+            {
+                var action = new Actions(driver);
+                action.MoveToElement(element).Perform();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(string.Format("Unable to move to element {0}", label), ex);
+            }
+        }
+
+        #endregion
+    }
+}
